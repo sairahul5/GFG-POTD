741f136e9a05d8478f44e4474a6a5f4fa27e34a6
sairahul5 <sairahuladabala123@gmail.com>
2026-06-06 19:51:40 +0530

    Day 001 | Non-Attacking Black and White Knights  | Medium ✅

--- GFG/2026/May/Day001.java
+++ GFG/2026/May/Day001.java
@@ -1,8 +1,30 @@
-package gfg.potd.y2026.may;
+package GFG.2026.May;
 
 public class Day001 {
-    boolean isSumOfConsecutive(int n) {
-        // code here
-        return (n&(n-1)) != 0;
+    class Solution {
+    
+    static long attackCount(int n,int m){
+        int a[] = {-2,-2,-1,-1,1,1,2,2};
+        int b[] = {-1,1,-2,2,-2,2,-1,1};
+        
+        int attack = 0;
+        
+        for(int r=0;r<n;r++){
+            for(int c=0;c<m;c++){
+                for(int k=0;k<8;k++) {
+                    int nr=r+a[k];
+                    int nc=c+b[k];
+                    if(nr>=0 && nr<n && nc>=0 && nc<m) attack++;
+                }
+            }
+        }
+        
+        return attack;
     }
+    public long numOfWays(int n, int m) {
+        long cells = 1L*n*m;
+        long total = cells*(cells-1);
+        return total-attackCount(n,m);
+    }
+}
 }
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # 🔥 GFG POTD Solutions
-Daily Problem of the Day solutions — GeeksforGeeks
+
+Daily solutions for GeeksforGeeks Problem of the Day (POTD) in Java.
 
 | Day | Problem | Difficulty | Solution |
 |-----|---------|------------|----------|
-| 001 | Problem Name | Easy | [Java](./2026/May/Day001.java) |
+| 001 | Problem Name | Easy | [Java](GFG/2026/May/Day001.java) |
+| 002 | Max Product Subset | Medium | [Java](GFG/2026/May/Day002.java) |
+
+## 📊 Stats
+- Language: Java
+- Platform: GeeksforGeeks
+- Solutions Updated Daily
